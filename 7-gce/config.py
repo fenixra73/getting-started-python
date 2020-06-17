ec4fc64f28c321d53dbe5eb5773d841a0c662afb
fenixra73 <fenixra73@gmail.com>
2020-06-18 01:15:17 +0300

    fix

--- 7-gce/config.py
+++ 7-gce/config.py
@@ -32,18 +32,18 @@ DATA_BACKEND = 'cloudsql'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'bookshelf-280017'
+PROJECT_ID = 'bookshelf-new'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
 # instance.
-CLOUDSQL_USER = 'root'
-CLOUDSQL_PASSWORD = 'password'
+CLOUDSQL_USER = 'user-db'
+CLOUDSQL_PASSWORD = 'S3MeisojIrhI2qfpGlDW5g=='
 CLOUDSQL_DATABASE = 'bookshelf'
 # Set this value to the Cloud SQL connection name, e.g.
 #   "project:region:cloudsql-instance".
 # You must also update the value in app.yaml.
-CLOUDSQL_CONNECTION_NAME = 'bookshelf-280017:us-central1:db-bookshelf'
+CLOUDSQL_CONNECTION_NAME = 'bookshelf-new:us-central1:db-bookshelf-db'
 
 # The CloudSQL proxy is used locally to connect to the cloudsql instance.
 # To start the proxy, use:
@@ -90,7 +90,7 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'bookshelf-store'
+CLOUD_STORAGE_BUCKET = 'bookshelf-pic-storage'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
